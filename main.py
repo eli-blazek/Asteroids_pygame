862a818ab8ef0df5da423509c8b09a720fd40192
Eli Blazku <eli.blazek20@gmail.com>
2024-09-02 16:34:06 +0200

    Sets display mod, attempts to fill the screen. Fails.

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import pygame
 from constants import *
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    pygame.display.set_mode(size=[SCREEN_WIDTH, SCREEN_HEIGHT])
+    while True:
+        pygame.Surface.fill(pygame.Color(0, 0, 0), rect=None, special_flags=0) # DOES NOT CURRENTLY WORK
 
 
 if __name__ == "__main__":
