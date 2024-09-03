2e4f605e1f4dd80d34659dbc10b6f0fced496254
Eli Blazku <eli.blazek20@gmail.com>
2024-09-03 08:50:11 +0200

    Things that did not work prevously now work.

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ from constants import *
 
 def main():
     pygame.init()
-    pygame.display.set_mode(size=[SCREEN_WIDTH, SCREEN_HEIGHT])
+    screen = pygame.display.set_mode(size=[SCREEN_WIDTH, SCREEN_HEIGHT])
     while True:
-        pygame.Surface.fill(pygame.Color(0, 0, 0), rect=None, special_flags=0) # DOES NOT CURRENTLY WORK
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(pygame.Color(0, 0, 0))
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
